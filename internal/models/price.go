d2a1a5b496f5fff2f4e22cbaff61776bbcd0f452
Shashank.M.S <145461478+Hawks0n@users.noreply.github.com>
2026-01-05 09:17:42 +0530

    Update price.go

--- internal/models/price.go
+++ internal/models/price.go
@@ -1,4 +1,7 @@
 package models
 
-// CryptoPrice represents dynamic crypto price JSON
+// CryptoPrice maps dynamic JSON responses like:
+// {
+//   "bitcoin": { "usd": 43000 }
+// }
 type CryptoPrice map[string]map[string]float64
